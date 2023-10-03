de64df57e0af4005d3bdaaa5c350f3d557f9d8ae
Ambareesh Shyam Sundar <ambareesh@gatech.edu>
2023-10-03 16:07:01 -0400

    add course critique wrapper endpoints

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,16 @@
-use axum::{response::Json, routing::get, Router};
+use axum::{routing::get, routing::post, Router};
 
-use serde_json::{json, Value};
+mod routes;
+
+use routes::{get_course_gpa, get_professor_gpa, get_professor_or_course};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/course", get(get_course_gpa))
-        .route("/professor", get(get_professor_gpa));
+        // Wrappers around Course Critique (critique.gatech.edu) API.
+        .route("/critique/course", post(get_course_gpa))
+        .route("/critique/professor", post(get_professor_gpa))
+        .route("/critique/search", post(get_professor_or_course));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -14,40 +18,3 @@ async fn main() {
         .unwrap();
 }
 
-async fn get_course_gpa(Json(payload): Json<Value>) -> Json<Value> {
-    let mut course_gpas = Value::Null;
-    if let Value::Object(payload_fields) = payload {
-        if let Value::String(course_id) = payload_fields.get("course_id").unwrap() {
-            course_gpas = json! {
-                reqwest::get(
-                    format!("https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/test/data/course?courseID={}", course_id)
-                )
-                .await.unwrap()
-                .json::<Value>()
-                .await.unwrap()
-            };
-        } else {
-            course_gpas = json!({ "data": 1 });
-        }
-    }
-    Json(course_gpas)
-}
-
-async fn get_professor_gpa(Json(payload): Json<Value>) -> Json<Value> {
-    let mut professor_gpas = Value::Null;
-    if let Value::Object(payload_fields) = payload {
-        if let Value::String(professor_name) = payload_fields.get("professor_id").unwrap() {
-            professor_gpas = json! {
-                reqwest::get(
-                    format!("https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/test/data/prof?profID={}&by=section", professor_name)
-                )
-                .await.unwrap()
-                .json::<Value>()
-                .await.unwrap()
-            };
-        } else {
-            professor_gpas = json!({ "data": 1 });
-        }
-    }
-    Json(professor_gpas)
-}
