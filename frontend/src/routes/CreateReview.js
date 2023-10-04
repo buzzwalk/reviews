d724eadd64dff3e64177c9087ba41edda383ed19
destevae <elainetang214@gmail.com>
2023-10-04 15:22:30 -0400

    Created the DropdownComponent.js file and made changes to it

--- frontend/src/routes/CreateReview.js
+++ frontend/src/routes/CreateReview.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import DropdownComponent from '../components/DropdownComponent'
 const CreateReview = () => {
-  return <p>this is the page to create reviews</p>
+  return (
+    <>
+      <p>this is the page to create reviews</p>
+      <DropdownComponent>
+      </DropdownComponent>
+    </>
+  );
+    
 }
 
 export default CreateReview
