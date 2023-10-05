b1de88bce941c61363c8abcbf08a3c87adf7fd90
kathryn.metheny <kathryn.metheny@gatech.edu>
2023-10-05 15:06:35 -0400

    Conditionally renders review type

--- frontend/src/routes/CreateReview.js
+++ frontend/src/routes/CreateReview.js
@@ -1,6 +1,46 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import ApartmentReview from '../components/ApartmentReview';
+import DiningReview from '../components/DiningReview';
+import ClassReview from '../components/ClassReview';
+
+import { ChakraProvider, Box, Text, ButtonGroup, Button } from '@chakra-ui/react';
+
+
 const CreateReview = () => {
-  return <p>this is the page to create reviews</p>
+  const [selectedReviewType, setSelectedReviewType] = useState(null);
+  return (
+    <ChakraProvider>
+      <Box p={4}>
+        <Text fontSize="xl" mb={4}>
+          This is the page to create reviews
+        </Text>
+        <ButtonGroup>
+          <Button
+            onClick={() => setSelectedReviewType('apartment')}
+            colorScheme={selectedReviewType === 'apartment' ? 'teal' : 'gray'}          
+          >
+            Apartment
+          </Button>
+          <Button
+            onClick={() => setSelectedReviewType('dining')}
+            colorScheme={selectedReviewType === 'dining' ? 'teal' : 'gray'}           
+          >
+            Dining
+          </Button>
+          <Button
+            onClick={() => setSelectedReviewType('class')}
+            colorScheme={selectedReviewType === 'class' ? 'teal' : 'gray'}           
+          >
+            Class
+          </Button>
+        </ButtonGroup>
+        {selectedReviewType === 'apartment' && <ApartmentReview />}
+        {selectedReviewType === 'dining' && <DiningReview />}
+        {selectedReviewType === 'class' && <ClassReview />}
+      </Box>
+    </ChakraProvider>
+  )
 }
 
 export default CreateReview
