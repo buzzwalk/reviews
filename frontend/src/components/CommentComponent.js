92e643c72c55855fd507fcf38f615987972041a7
Nicholas Suh <nicksuh17@gmail.com>
2023-10-17 18:03:29 -0400

    added react stuff

--- frontend/src/components/CommentComponent.js
+++ frontend/src/components/CommentComponent.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./Textbox.css"
 import logo from "./Gt-seal.png"
+import { useState } from "react"
 function CommentComponent() {
-
+    const [formData, setFormData] = useState("")
     
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData);
+    }
     return (
         <>
         <center> <h1 className = "header"> BUZZWALK COMMENTS </h1> </center>
@@ -11,16 +16,17 @@ function CommentComponent() {
 
         <div className = "division">
             <form>
-                <textarea rows = "4" cols = "50" placeholder = "Enter Your Comment...">
+                <textarea rows = "4" cols = "50" placeholder = "Enter Your Comment..." onChange={(e) => setFormData(e.target.value)}>
                 </textarea>
+                <label>
+                    <button className = "submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+                </label> 
             </form>    
         </div>
 
         <div className = "division">
             <form>  
-                <label>
-                    <button className = "submit" >Submit</button>
-                </label>  
+                 
             </form>
         </div>
 
