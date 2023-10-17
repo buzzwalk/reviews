5f08c74c8023fb42448050bb4c5bee2af3716a1b
Sam <bills2021e@gmail.com>
2023-10-17 19:13:44 -0400

    Add NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import SearchBar from './components/homepage/SearchBar'
+import NavBar from './components/homepage/NavBar'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <header className='App-header'>
         <Link to='/review'>Add review</Link>
         <SearchBar />
+        <NavBar />
       </header>
     </ChakraProvider>
   )
