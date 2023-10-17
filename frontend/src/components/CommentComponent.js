655a9855a6bd38182568e8f0b3ecbbb5025b9720
Nicholas Suh <nicksuh17@gmail.com>
2023-10-17 17:13:41 -0400

    minor fixes

--- frontend/src/components/CommentComponent.js
+++ frontend/src/components/CommentComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Textbox.css"
 import logo from "./Gt-seal.png"
-const CommentComponent = () => {
+function CommentComponent() {
+
+    
     return (
         <>
-        {Comment /* <img className = "buzzlogo" src = "https://news.gatech.edu/sites/default/files/hg_media/tower_green.jpg"/> */}
-        <center> <h1 className = "header"> BUZZPORT COMMENTS </h1> </center>
+        <center> <h1 className = "header"> BUZZWALK COMMENTS </h1> </center>
         {/*Comment <img className = "logo" src = {logo} />*/}
 
         <div className = "division">
@@ -16,27 +17,19 @@ const CommentComponent = () => {
         </div>
 
         <div className = "division">
-            <form action = "#" method = "post">  
-            <label>
-                <input className = "submit" value= "Submit"/>
-            </label>  
+            <form>  
+                <label>
+                    <button className = "submit" >Submit</button>
+                </label>  
             </form>
         </div>
 
         <div className = "division">
             <img className = "logo" src = {logo} />
-            {/*<img className = "buzzlogo" src = "https://ramblinwreck.com/imgproxy/wEpLPwno24G0Klmjfv9DNLGJIKhXoFgc24UbKDjmXb0/fit/2500/2500/ce/0/aHR0cHM6Ly9yYW1ibGlud3JlY2suY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDIzLzA1L0J1enpPbldoaXRlLmpwZw.jpg" /> */}
-        </div>
-
-        <div>
-            <footer id = "footer"> 
-            All information submitted can and will be used against you in a court of law. 
-            God Bless the Georgia Institute of Technology.
-            </footer>
         </div>
 
         </>
-    );
+    )
 }
 
 export default CommentComponent
