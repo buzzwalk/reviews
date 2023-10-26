21d12fde44b8b6fee251d42329e36e298a1a1fc7
Ambareesh Shyam Sundar <ambareesh@gatech.edu>
2023-10-26 17:24:05 -0400

    scraping works better except if credit hours is fractional

--- server/src/scraper/mod.rs
+++ server/src/scraper/mod.rs
@@ -7,17 +7,25 @@ pub async fn scrape_course_catalog() -> WebDriverResult<Json<Value>> {
     let caps = DesiredCapabilities::firefox();
     let driver = WebDriver::new("http://localhost:4444", caps).await?;
 
-    // Navigate to https://wikipedia.org.
     driver.goto("https://catalog.gatech.edu/coursesaz/").await?;
     let a_to_z_index = driver.find(By::Id("atozindex")).await?;
 
     let department_pages = a_to_z_index.query(By::Tag("a")).all().await?;
+    let mut page_links: Vec<Option<String>> = vec![];
 
     for page in department_pages {
-        let page_link = page.attr("href").await?;
+        page_links.push(page.attr("href").await?);
+    }
+
+    for page_link in page_links {
+        // let page_link = page.attr("href").await?;
         if let Some(link) = page_link {
             driver.goto(link).await?;
-            let course_descs = driver.find(By::ClassName("sc_sccoursedescs")).await?;
+            let course_descs = driver.find(By::ClassName("sc_sccoursedescs")).await;
+            let course_descs = match course_descs {
+                Ok(e) => e,
+                Err(_) => continue, 
+            };
             let department_courses = course_descs
                 .query(By::ClassName("courseblock"))
                 .all()
@@ -30,21 +38,23 @@ pub async fn scrape_course_catalog() -> WebDriverResult<Json<Value>> {
                     .await?;
 
                 let title_text = title.text().await?;
-                let mut title_fields = title_text.split('.');
+                let mut title_fields = title_text.split('.').collect::<Vec<&str>>();
+                title_fields.pop();
+                // println!("{:?}", title_fields);
 
                 course_map.insert(
                     String::from("course_id"),
-                    Value::String(title_fields.next().unwrap().to_string()),
+                    Value::String(title_fields[0].to_string()),
                 );
                 course_map.insert(
                     String::from("course_title"),
-                    Value::String(title_fields.next().unwrap().get(1..).unwrap().to_string()),
+                    Value::String(title_fields[1..(title_fields.len() - 1)].join("")[1..].to_string()),
                 );
+                // println!("{}", title_fields.pop().unwrap());
                 course_map.insert(
                     String::from("course_ch"),
-                    // Value::String(title_fields.next().unwrap().to_string()),
                     Value::Number(Number::from(
-                        u8::from_str_radix(title_fields.next().unwrap().get(1..2).unwrap(), 10)
+                        u8::from_str_radix(title_fields.pop().unwrap().get(1..2).unwrap(), 10)
                             .unwrap(),
                     )),
                 );
