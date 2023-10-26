76713b2cc84536caaeca8a16834bb0b3801841a6
Stanley Wong <stanley0620@gmail.com>
2023-10-26 15:12:23 -0400

    when publish comment on site, will add to firebase backend

--- frontend/src/components/CommentComponent.js
+++ frontend/src/components/CommentComponent.js
@@ -2,21 +2,31 @@ import React from 'react'
 import "./Textbox.css"
 import logo from "../images/Gt-seal.png"
 import { useState } from "react"
+import db from "../firebase"
+import { setDoc, doc } from "firebase/firestore"; 
+
 function CommentComponent() {
-    const [formData, setFormData] = useState("")
+
+    const [formData, setFormData] = useState({
+        comment: '',
+
+    });
     
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(formData);
+
+        const docRef = setDoc(doc(db, "DiningHalls", "Willage", "Reviews", "Comment"), {
+            comment: formData.comment
+        });
     }
+    
     return (
         <>
         <center> <h1 className = "header"> BUZZWALK COMMENTS </h1> </center>
-        {/*Comment <img className = "logo" src = {logo} />*/}
 
         <div className = "division">
             <form>
-                <textarea rows = "4" cols = "50" placeholder = "Enter Your Comment..." onChange={(e) => setFormData(e.target.value)}>
+                <textarea rows = "4" cols = "50" placeholder = "Enter Your Comment..." onChange={(e) => setFormData({ comment: e.target.value })}>
                 </textarea>
                 <label>
                     <button className = "submit" onClick={(e) => handleSubmit(e)}>Submit</button>
