401d5d53f2f5f58a98f911f848523ce4f6c8c8c8
ecai32 <ecai32@gatech.edu>
2023-10-26 16:05:48 -0400

    Connected submit button to firebase

--- frontend/src/components/CommentComponent.js
+++ frontend/src/components/CommentComponent.js
@@ -1,41 +1,59 @@
-import React from 'react'
-import "./Textbox.css"
-import logo from "../images/Gt-seal.png"
-import { useState } from "react"
+import React, { useState } from 'react';
+import "./Textbox.css";
+import logo from "../images/Gt-seal.png";
+import db from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+
 function CommentComponent() {
-    const [formData, setFormData] = useState("")
-    
+    const [formData, setFormData] = useState({
+        comment: ''
+    });
+
     function handleSubmit(e) {
-        e.preventDefault()
-        console.log(formData);
+        e.preventDefault();
+
+        const docRef = doc(db, "DiningHalls", "Willage", "Reviews", "Comment");
+        setDoc(docRef, {
+            comment: formData.comment
+        })
+            .then(() => {
+                console.log("Comment successfully added to Firebase!");
+            })
+            .catch((error) => {
+                console.error("Error adding comment to Firebase: ", error);
+            });
     }
+
     return (
         <>
-        <center> <h1 className = "header"> BUZZWALK COMMENTS </h1> </center>
-        {/*Comment <img className = "logo" src = {logo} />*/}
+            <center> <h1 className="header"> BUZZWALK COMMENTS </h1> </center>
 
-        <div className = "division">
-            <form>
-                <textarea rows = "4" cols = "50" placeholder = "Enter Your Comment..." onChange={(e) => setFormData(e.target.value)}>
-                </textarea>
-                <label>
-                    <button className = "submit" onClick={(e) => handleSubmit(e)}>Submit</button>
-                </label> 
-            </form>    
-        </div>
+            <div className="division">
+                <form>
+                    <textarea
+                        rows="4"
+                        cols="50"
+                        placeholder="Enter Your Comment..."
+                        onChange={(e) => setFormData({ comment: e.target.value })}
+                    />
+                    <label>
+                        <button className="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+                    </label>
+                </form>
+            </div>
 
-        <div className = "division">
-            <form>  
-                 
-            </form>
-        </div>
+            <div className="division">
+                <form>
 
-        <div className = "division">
-            <img className = "logo" src = {logo} />
-        </div>
+                </form>
+            </div>
 
+            <div className="division">
+                <img className="logo" src={logo} alt="Logo" />
+            </div>
         </>
-    )
+    );
 }
 
-export default CommentComponent
+export default CommentComponent;
