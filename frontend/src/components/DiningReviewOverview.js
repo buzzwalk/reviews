193e4b24b0834d0a48d24eacce855c7e7a2971be
ecai32 <ecai32@gatech.edu>
2023-10-30 15:01:16 -0400

    Displayed data from firebase

--- frontend/src/components/DiningReviewOverview.js
+++ frontend/src/components/DiningReviewOverview.js
@@ -1,34 +1,54 @@
-import "../style/ReviewOverview.css"
-import Navbar from "./navbar.js"
-import { useEffect } from "react"
-import PreviewCard from "./PreviewCard"
-import data from "../sampleDiningData"
-export default function DiningReviewOverview() {
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "../firebase";
+import Navbar from "./navbar.js";
+import PreviewCard from "./PreviewCard";
+
+const dormsRef = collection(db, "DiningHalls");
+
+export default function ApartmentReviewOverview() {
+    const [previewCards, setPreviewCards] = useState([]);
 
     function getPreviewCards(){
-        const dataArr = data.data;
-        return dataArr.map((e) =>
-            <PreviewCard name={e.name} subheading={e.address} rating={e.rating}/>
-        )
+        return previewCards.map((card) => (
+            <PreviewCard
+                name={card.name}
+                subheading={card.address}
+                rating={card.overallRating}
+            />
+        ))
     }
-    return(
+
+    useEffect(() => {
+        const fetchApartments = async () => {
+            const querySnapshot = await getDocs(dormsRef);
+            const apartmentReviews = querySnapshot.docs.map((doc) => ({
+                name: doc.data().name,
+                address: doc.data().address,
+                overallRating: doc.data().overallRating,
+            }));
+            
+            setPreviewCards(apartmentReviews);
+        };
+
+        fetchApartments();
+    }, []);
+
+    return (
         <>
-        <Navbar />   
-        <div className="reviewoverview">
-            <h1>Dining Halls</h1>
-            <div className="main">
-                
-                <div className="filters">
-                    <h3>Filters</h3>
-                    <p>filters here</p>
-                </div>
-                <div className="previews">
-                    {getPreviewCards()}  
+            <Navbar />
+            <div className="reviewoverview">
+                <h1>Apartments</h1>
+                <div className="main">
+                    <div className="filters">
+                        <h3>Filters</h3>
+                        <p>filters here</p>
+                    </div>
+                    <div className="previews">
+                        {getPreviewCards()}
+                    </div>
                 </div>
             </div>
-            
-        </div>
         </>
-        
-    )
+    );
 }
