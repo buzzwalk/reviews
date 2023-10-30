b214a8d15dea669c25d200fafcb9709063ceebc7
Sam <bills2021e@gmail.com>
2023-10-29 23:35:30 -0400

    Write filter component for class and professor

---
+++ frontend/src/components/ClassProfessorFilter.js
@@ -0,0 +1,61 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Card, CardBody, Select, RangeSlider, RangeSliderFilledTrack, RangeSliderTrack, 
+    RangeSliderThumb, FormControl, FormLabel, Heading } from "@chakra-ui/react";
+import { Form } from "react-router-dom";
+
+export default function ClassProfessorFilter({ setDepartment, setRatingCategory, setRatingRange }) {
+    
+    function handleDepartmentChange(event) {
+        setDepartment(event.value);
+    }
+
+    function handleRatingCategoryChange(event) {
+        setRatingCategory(event.value);
+    }
+
+    function handleRatingRangeChange(event) {
+        setRatingRange(event.value)
+    }
+
+    return (
+        <Form style={{backgroundColor: "grey"}}>
+            <Heading as="h2" size="lg" noOfLines={1}>Filters</Heading>
+            <FormControl>
+                <FormLabel>Department</FormLabel>
+                <Select placeholder="Department" onChange={(e) => handleDepartmentChange(e)}>
+                    <option value="Computer Science">Computer Science</option>
+                    <option value="Other">Need to get other departments for dropdown</option>
+                </Select>
+            </FormControl>
+            <FormControl>
+                <FormLabel>Rating Category</FormLabel>
+                <Select placeholder="Rating Category" onChange={(e) => handleRatingCategoryChange(e)}>
+                    <option value="Overall">Overall</option>
+                    <option value="Easiness">Easiness</option>
+                    <option value="Clarity">Clarity</option>
+                    <option value="Workload">Workload</option>
+                    <option value="Helpfulness">Helpfulness</option>
+                </Select>
+            </FormControl>
+            <FormControl>
+                <FormLabel>Rating Range</FormLabel>
+                <RangeSlider defaultValue={[120, 240]} min={0} max={300} step={30} colorScheme="pink">
+                    <RangeSliderTrack bg='red.100'>
+                        <RangeSliderFilledTrack bg='tomato' />
+                    </RangeSliderTrack>
+                    <RangeSliderThumb boxSize={6} index={0} />
+                    <RangeSliderThumb boxSize={6} index={1} />
+                </RangeSlider>
+            </FormControl>
+        </Form>
+    );
+
+}
+
+ClassProfessorFilter.propTypes = {
+    name: PropTypes.func,
+    route: PropTypes.func,
+    imgSrc: PropTypes.func
+}
--- frontend/src/components/DiningReviewOverview.js
+++ frontend/src/components/DiningReviewOverview.js
@@ -1,34 +1,38 @@
 import "../style/ReviewOverview.css"
 import Navbar from "./navbar.js"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import PreviewCard from "./PreviewCard"
 import data from "../sampleDiningData"
-export default function DiningReviewOverview() {
+import ClassProfessorFilter from "./ClassProfessorFilter"
 
-    function getPreviewCards(){
+export default function DiningReviewOverview() {
+    const [ department, setDepartment ] = useState();
+    const [ ratingCategory, setRatingCategory ] = useState();
+    const [ ratingRange, setRatingRange ] = useState();
+
+    function getPreviewCards() {
         const dataArr = data.data;
-        return dataArr.map((e) =>
-            <PreviewCard name={e.name} subheading={e.address} rating={e.rating}/>
-        )
+        return dataArr.map((e) => (<PreviewCard key={e.name} name={e.name} subheading={e.address} rating={e.rating}/>));
     }
-    return(
+
+    return (
         <>
-        <Navbar />   
-        <div className="reviewoverview">
-            <h1>Dining Halls</h1>
-            <div className="main">
+            <Navbar />   
+            <div className="reviewoverview">
+                <h1>Dining Halls</h1>
+                <div className="main">
+                    <ClassProfessorFilter 
+                        setDepartment={setDepartment} 
+                        setRatingCategory={setRatingCategory} 
+                        setRatingRange={setRatingRange} 
+                    />
+                    <div className="previews">
+                        {getPreviewCards()}  
+                    </div>
+                </div>
                 
-                <div className="filters">
-                    <h3>Filters</h3>
-                    <p>filters here</p>
-                </div>
-                <div className="previews">
-                    {getPreviewCards()}  
-                </div>
             </div>
-            
-        </div>
         </>
         
-    )
+    );
 }
