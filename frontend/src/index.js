9a9857c25d6b9eb1424f6f78157dfc248ea84993
Leo Jiang <leojiang.ca@gmail.com>
2023-10-31 19:04:33 -0400

    guiiu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,7 @@ import ProfessorsReviewOverview from './components/ProfessorsReviewOverview'
 import ClassesReviewOverview from './components/ClassesReviewOverview'
 import ApartmentReviewOverview from './components/ApartmentReviewOverview'
 import DiningReviewOverview from './components/DiningReviewOverview'
-//yest
+//yests
 const router = createBrowserRouter([
   {
     path: '/',
