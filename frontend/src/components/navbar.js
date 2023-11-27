b564d7d739acff18673e6e2a068335db62ecae54
Nicholas Suh <nicksuh17@gmail.com>
2023-11-27 17:51:08 -0500

    nav search bar (classes)

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,4 +1,4 @@
-import { Box, Flex, HStack, Input, InputGroup, InputRightElement, InputLeftElement, Select, VStack, FormControl } from "@chakra-ui/react"
+import { Box, Flex, HStack, Input, InputGroup, InputRightElement, InputLeftElement, Select, VStack, FormControl, position } from "@chakra-ui/react"
 import "../style/navbar.css"
 import { SearchIcon, StarIcon } from "@chakra-ui/icons"
 import { VscAccount } from "react-icons/vsc";
@@ -9,59 +9,67 @@ import {
     AutoCompleteItem,
     AutoCompleteList,
   } from "@choc-ui/chakra-autocomplete";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import algoliasearch from 'algoliasearch/lite';
+const searchClient = algoliasearch('N39JIC33WP', 'de58da0111cf638279244fc3374b674a');
 
 export default function Navbar() {
     
-    const [suggested, setSuggested] = useState(['nigeria', 'korea', 'Japan'])
-    const [reviewable, setReviewable] = useState('DiningHalls');
-    console.log(reviewable);
+    const [search, setSearch] = useState(' ');
+    
+    const [suggested, setSuggested] = useState(null)
+    const [reviewable, setReviewable] = useState('classes');
+   
+    
+    useEffect(() => {
+        let index = 0;
+        searchClient.initIndex(reviewable).search(search).then(({hits}) =>{
+            setSuggested(hits)
+        })
+    }, [search, reviewable])
+
+ 
+
+    console.log(suggested)
     return(
-        <Flex justifyContent="space-between" alignItems="center"  bg="#333333" marginLeft="20px" marginRight="20px" marginTop={"15px"} height="50px" borderRadius="20px" paddingLeft={"15px"} paddingRight={"15px"}>
+        <Flex style={{position:"relative", zIndex:"10"}} justifyContent="space-between" alignItems="center"  bg="#333333" marginLeft="20px" marginRight="20px" marginTop={"15px"} height="50px" borderRadius="20px" paddingLeft={"15px"} paddingRight={"15px"}>
             <StarIcon color="#959595"></StarIcon> {/*placeholder */}
             {/* <InputGroup width={"40%"} >
                 <InputLeftElement>
                     <SearchIcon color="gray.300" ></SearchIcon>
                 </InputLeftElement>
-                <Input outline={"solid 1px #54585A"} background="#333333" borderColor="#333333" type="text" ></Input>
+                <Input onChange={(e)=>setSearch(e.target.value)} outline={"solid 1px #54585A"} background="#333333" borderColor="#333333" type="text" ></Input>
                 <InputRightElement width={"fit-content"}>
-                    <Select  background="#222222" color="#959595" variant="filled" height={"30px"} marginRight={"10px"}>
-                        <option style={{background: "#333333"}}>DINING</option>
-                        <option style={{background: "#333333"}}>HOUSING</option>
-                        <option style={{background: "#333333"}}>PROFS</option>
-                        <option style={{background: "#333333"}}>CLASSES</option>
+                    <Select onChange={(e)=>(setReviewable(e.target.value))} background="#222222" color="#959595" variant="filled" height={"30px"} marginRight={"10px"}>
+                        <option value="DiningHalls" style={{background: "#333333"}}>DINING</option>
+                        <option value="Dorms" style={{background: "#333333"}}>HOUSING</option>
+                        <option value="Professors" style={{background: "#333333"}}>PROFS</option>
+                        <option value="classes" style={{background: "#333333"}}>CLASSES</option>
                     </Select>
                 </InputRightElement>
             </InputGroup> */}
             <HStack width="50%" maxWidth={"500px"}>
                 <InputGroup width="100%">
-                    {/* <InputLeftElement>
-                        <SearchIcon color="gray.300" ></SearchIcon>
-                    </InputLeftElement> */}
-                    <AutoComplete >
                     
-                        <AutoCompleteInput variant="filled"/>
-                        <AutoCompleteList>
-                            {suggested.map((country, cid) => (
-                                <AutoCompleteItem
-                                key={'option-${cid}'}
-                                value={country}
+                    <AutoComplete >
+                        <AutoCompleteInput variant="filled" onChange={(e)=>setSearch(e.target.value)}/>
+                        <AutoCompleteList style={{position: "relative", zIndex:"10", background: "#333333"}}>
+                            {suggested != null && suggested.map((hit, cid) => (
+                            <AutoCompleteItem 
+                                value={hit.objectID}
                                 textTransform="capitalize"
-                                onSelect={()=>{console.log("test")}}
-                                width={"50"}
-                                >
-                                    {country}
-                                </AutoCompleteItem>
+                            >
+                                {hit.objectID}
+                            </AutoCompleteItem>
                             ))}
                         </AutoCompleteList>
                     </AutoComplete>
                     <InputRightElement width={"fit-content"}>
                             <Select onChange={(e)=>(setReviewable(e.target.value))} width="fit-content" background="#222222" color="#959595" variant="filled" height={"30px"} marginRight={"9px"}>
+                                <option value="classes" style={{background: "#333333"}}>CLASSES</option>
                                 <option value="DiningHalls" style={{background: "#333333"}}>DINING</option>
                                 <option value="Dorms" style={{background: "#333333"}}>HOUSING</option>
                                 <option value="Professors" style={{background: "#333333"}}>PROFS</option>
-                                <option value="Classes" style={{background: "#333333"}}>CLASSES</option>
                             </Select>
                         </InputRightElement>
                     
