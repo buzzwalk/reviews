14a88d064ba504ad58e165d9ffc1bb4ce7203de5
Nicholas Suh <nicksuh17@gmail.com>
2023-11-28 15:57:01 -0500

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,7 +22,7 @@ import ViewProfessorReviewInfo from './components/reviews/ViewProfessorReviewInf
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/reviews',
     element: <App />,
   },
   {
